13ad8e65c0eb1432ac6eb3ad3ddffa831c77aec3
sahrul <sahrul@gmail.com>
2021-12-19 15:24:14 +0700

    frist commit

---
+++ application/models/CRUD.php
@@ -0,0 +1,7 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+ini_set('date.timezone', 'Asia/Jakarta');
+
+class CRUD extends CI_Model
+{
+}
---
+++ application/views/v_home.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- Sweet Alert 2 -->
+    <script src="<?= base_url('assets'); ?>/plugin/sweetalert2/dist/sweetalert2.all.min.js"></script>
+    <link rel="stylesheet" href="<?= base_url('assets'); ?>/plugin/sweetalert2/dist/sweetalert2.min.css">
+    <!-- Bootstrap 5 -->
+    <link rel="stylesheet" href="<?= base_url('assets'); ?>/plugin/bootstrap5/css/bootstrap.min.css">
+
+    <!-- CSS RPTRA -->
+    <link rel="stylesheet" href="<?= base_url('assets'); ?>/css/rptra.css">
+</head>
+
+<body>
+    <header class="d-flex justify-content-center align-items-center text-center">
+        <img src="<?= base_url('assets'); ?>/img/icon3.png" alt="header">
+    </header>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg" id="navbar">
+        <div class="container-fluid">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link fw-bold" aria-current="page" href="#">HOME</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link fw-bold" href="#">PROFILE</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle fw-bold" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            POKJA I
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle fw-bold" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            POKJA II
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle fw-bold" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            POKJA III
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle fw-bold" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            POKJA IV
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle fw-bold" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            EKONOMI KREATIF
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="hero " style="background-image: url(<?= base_url('assets/img/hero.jpg'); ?>);">
+    </div>
+    <section id="content" style="position:relative;">
+        <div id="content-isi" class="container">
+            <div class="row row-cols-1 row-cols-lg-2 border border-danger mt-2">
+                <div class="col col-lg-8 shadow rounded mt-3" id="berita -terbaru">
+                    <div class="judul-content bg-success py-2 px-4 rounded">
+                        <h2 class="text-white">berita terbaru</h2>
+                    </div>
+                    <div class="isi px-4 py-2">
+                        <h4 class="fw-bold py-2">Judul Berita</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam voluptate tempora aliquam consequuntur accusamus saepe delectus laborum illum adipisci nostrum sapiente officia repellendus accusantium maxime, nesciunt molestias corrupti soluta provident natus optio quibusdam voluptatem! Pariatur possimus qui hic accusamus ab veniam facilis voluptatum earum obcaecati voluptas, nulla aliquam ea minus labore sint non laborum ipsam libero fugiat. Recusandae pariatur, rerum officia voluptatum obcaecati unde quaerat, inventore assumenda aperiam vero asperiores alias totam, rem laudantium eaque cupiditate saepe. Debitis harum tenetur error consectetur et provident, non vel, voluptatum, accusantium aliquam aliquid cum. Alias incidunt unde consequuntur tenetur quis fugiat obcaecati culpa, perferendis aut dolorum at quaerat ipsa odio doloribus excepturi quo minus officia. Aspernatur voluptatibus illo, iusto aut possimus pariatur in cum consequatur. Provident fugiat quae eaque ullam magni perspiciatis autem illo reiciendis, doloremque distinctio libero hic corrupti repellendus consequatur dolores iure officia architecto consectetur omnis animi voluptatem nulla quis ad. Accusantium numquam dolorum quae eum tenetur quaerat, ipsam quidem officia pariatur amet? Magni explicabo sunt laboriosam, repudiandae facere tempora nostrum assumenda iusto excepturi aliquam temporibus reprehenderit omnis illum odio officiis, cum suscipit illo, corporis placeat dolorem? Temporibus officia vitae dolore voluptate blanditiis quos dolorem, deserunt at vel cumque inventore. Neque.</p>
+                    </div>
+                    <div class="row row-cols-1 row-cols-lg-3 row-cols-md-2 border border-warning" id="berita-lain">
+                        <div class="col">
+                            <div class="berita-lain card shadow my-2">
+                                <img src="<?= base_url('assets'); ?>/img/foto rptra cempaka/Monitoring.jpg" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="berita-lain card shadow my-2">
+                                <img src="<?= base_url('assets'); ?>/img/foto rptra cempaka/24296370_314313999052537_511373022351386049_n.jpg" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="berita-lain card shadow my-2">
+                                <img src="<?= base_url('assets'); ?>/img/foto rptra cempaka/slide1.jpg" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col col-lg-4 mt-3 shadow bg-primary" style="height: 1000px;">
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+
+    <script src="<?= base_url('assets'); ?>/plugin/bootstrap5/js/bootstrap.min.js"></script>
+    <script>
+        window.onscroll = function() {
+            myFunction()
+        };
+        var navbar = document.getElementById("navbar");
+        var sticky = navbar.offsetTop;
+
+        function myFunction() {
+            if (window.pageYOffset >= sticky) {
+                navbar.classList.add("fixed-top")
+            } else {
+                navbar.classList.remove("fixed-top");
+            }
+        }
+    </script>
+</body>
+
+</html>
